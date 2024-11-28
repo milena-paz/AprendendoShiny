bb044b3100d6c1faa94f7cdd1c8fcb1f7ef0f11f
milena-paz <milenap4zfreitas@gmail.com>
2024-11-28 18:35:11 -0300

    Update server.R
    o plot estava atualizando automaticamente, eu queria que funcionasse apenas quando pressionado o botao :x mas agora ta tudo certo

--- InterfaceMovel/InterfaceMovel/server.R
+++ InterfaceMovel/InterfaceMovel/server.R
@@ -3,10 +3,8 @@ library(ggplot2)
 
 function(input, output, session) {
 
-  observeEvent(input$button, {
-    output$plot1 <- renderPlot({
+  df <- eventReactive(input$button, {
       ggplot(data=mtcars,aes(x=.data[[input$x]],y=.data[[input$y]])) + geom_point(col="blue")
-      })
   })
-
+  output$plot1 <- renderPlot(df())
 }
