9f52a26d5aa2a2ddf8ecd800cd53b8d5cf98bdf4
milena-paz <milenap4zfreitas@gmail.com>
2024-11-29 10:23:44 -0300

    Update: server.R
    mais uma mudança estética, pontos agora serão pretos

--- InterfaceMovel/InterfaceMovel/server.R
+++ InterfaceMovel/InterfaceMovel/server.R
@@ -4,7 +4,7 @@ library(ggplot2)
 function(input, output, session) {
 
   df <- eventReactive(input$button, {
-      ggplot(data=mtcars,aes(x=.data[[input$x]],y=.data[[input$y]])) + geom_point(col="blue") +
+      ggplot(data=mtcars,aes(x=.data[[input$x]],y=.data[[input$y]])) + geom_point(col="black") +
       theme(
         panel.background = element_rect(fill='#e0afbb'),
         plot.background = element_rect(fill='#733b4c', color=NA),
