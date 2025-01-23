f1ab9d19026bcb0abd04baee8bdebde861514a88
milena-paz <milenap4zfreitas@gmail.com>
2025-01-23 11:09:12 -0300

    cont. mudanca anterior
    :p

--- InterfaceMovel/InterfaceMovel/server.R
+++ InterfaceMovel/InterfaceMovel/server.R
@@ -2,14 +2,16 @@ library(shiny)
 library(ggplot2)
 
 function(input, output, session) {
-
+  thematic::thematic_shiny()
+  
   df <- eventReactive(input$button, {
       ggplot(data=mtcars,aes(x=.data[[input$x]],y=.data[[input$y]])) + geom_point(col="red") +
       theme(
+        plot.background = element_blank(),
         axis.text = element_text(size=10),
         axis.ticks = element_line(),
         axis.title= element_text(size=15)
       )
   })
-  output$plot1 <- renderPlot(df(), bg="transparent")
+  output$plot1 <- renderPlot(df(),bg="transparent")
 }
